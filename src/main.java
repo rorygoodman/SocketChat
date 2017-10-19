8573b55c7f18ffbfde31b3365e7f2924cf6346a2
Rory Goodman <goodmanr@tcd.ie>
2017-10-19 14:54:06 +0100

    Added new class to implement protocol

--- src/main.java
+++ src/main.java
@@ -1,14 +1,19 @@
 import java.io.IOException;
 import java.net.*;
+import java.io.*;
 public class main {
 
 	public static void main(String[] args) {
 		int port = Integer.parseInt(args[0]);
 		try {
-			ServerSocket socket = new ServerSocket(port);
+			ServerSocket serverSocket = new ServerSocket(port);
+			Socket clientSocket = serverSocket.accept();
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
