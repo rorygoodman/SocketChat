80242595a9b62f3bbffa623ff52cf78e4ad98634
Rory Goodman <goodmanr@tcd.ie>
2017-10-20 15:08:25 +0100

    Nuked it and stole the oracle multithreaded client server example. Building from here.

--- src/main.java
+++ src/main.java
@@ -1,19 +1,27 @@
-import java.io.IOException;
+
+
+
+ 
 import java.net.*;
 import java.io.*;
+ 
 public class main {
-
-	public static void main(String[] args) {
-		int port = Integer.parseInt(args[0]);
-		try {
-			ServerSocket serverSocket = new ServerSocket(port);
-			Socket clientSocket = serverSocket.accept();
-			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
+    public static void main(String[] args) throws IOException {
+ 
+    if (args.length != 1) {
+        System.err.println("No port no. entered");
+        System.exit(1);
+    }
+    int portNumber = Integer.parseInt(args[0]);
+    boolean listening = true;
+     
+    try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+        while (listening) {
+            new ClientThread(serverSocket.accept()).start();
+        }
+    } catch (IOException e) {
+        System.err.println("Could not listen on port " + portNumber);
+        System.exit(-1);
+    }
+    }
 }
