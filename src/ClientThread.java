a149c8692a12b1e0b89b28561b1b6d2bcf2e7afe
goodmanr <goodmanr@2Y3ZM62.itserv.scss.tcd.ie>
2017-11-06 17:40:27 +0000

    Joining and leaving chatroom, connecting to multiple clients

--- src/ClientThread.java
+++ src/ClientThread.java
@@ -24,14 +24,13 @@ public class ClientThread extends Thread {
         	PrintWriter chatWriter = new PrintWriter("chat.txt", "UTF-8");
             String inputLine, outputLine,message;
             inputLine=null;
-            while (alive) {
-            	
+            while (alive) { 	
             	inputLine=in.readLine();
             	System.out.println(inputLine);
             	if(inputLine!=null){
             		if(inputLine.contains("HELO")){
        			
-            			outputLine= "HELO"+inputLine.substring("HELO".length())+"\nIP:134.226.50.34\nPort:1234\nStudentID:14316993";
+            			outputLine= "HELO"+inputLine.substring("HELO".length())+"\nIP: 134.226.50.42 \nPort:1234\nStudentID:14316993";
             			outStream.write(outputLine.getBytes());
             		}
             		else if(inputLine.contains("KILL_SERVICE")){
@@ -42,9 +41,11 @@ public class ClientThread extends Thread {
             		else if(inputLine.contains("JOIN_CHATROOM")){
             			chatroom = inputLine.substring(inputLine.indexOf("JOIN_CHATROOM: ")+15, inputLine.length());
             			inputLine=in.readLine();
+            			System.out.println(inputLine);
             			clientIP=inputLine.substring("CLIENT_IP: ".length());
-            			in.readLine();
+            			in.readLine();	
             			inputLine=in.readLine();
+            			System.out.println(inputLine);
             			
             			alias = inputLine.substring("CLIENT_NAME: ".length());
             			System.out.println(alias);
@@ -52,24 +53,20 @@ public class ClientThread extends Thread {
       						  "SERVER_IP: 134.226.50.34\n"+
     						  "PORT: 1234\n"+
     						  "ROOM_REF: 0\n"+
-    						  "JOIN_ID: 0\n\n";
+    						  "JOIN_ID: 0\n";
             			outStream.write(outputLine.getBytes());
-            			System.out.println("boop");
-            		}
-            		else if(inputLine.contains("LEAVE_CHATROOM: ")){
-            			if(inputLine.substring(inputLine.indexOf("LEAVE_CHATROOM: ")).equals(chatroom)){
-            				chatroom=null;
-            				outputLine="LEFT_CHATROOM: [ROOM_REF]\n"+
-            				"JOIN_ID: 0\n\n";
-            				outStream.write(outputLine.getBytes());
-            			}
+            			outputLine="CHAT:0\n"+
+            			"CLIENT_NAME: "+alias+"\n"+
+            					"MESSAGE: "+alias+" JOINED\n\n";
+            			outStream.write(outputLine.getBytes());
             		}
             		else if(inputLine.contains("CHAT:")){
-            			//inputLine=in.readLine();//TODO proper implementation
-            			//inputLine=in.readLine();//TODO
+            			inputLine=in.readLine();//TODO proper implementation
+            			inputLine=in.readLine();//TODO
             			message=inputLine;
             			while(!inputLine.contains("\n\n")){
             				inputLine=in.readLine();
+            				System.out.println(inputLine);
             				message+=inputLine;
             			}
             			outputLine="CHAT: "+chatroom+"\n"+
@@ -77,6 +74,22 @@ public class ClientThread extends Thread {
             			"MESSAGE: "+message;
             			System.out.println("gets here");
             			outStream.write(outputLine.getBytes());
+            			
+            		}
+            		else if(inputLine.contains("LEAVE_CHATROOM: ")){
+            			//if(inputLine.substring(inputLine.indexOf("LEAVE_CHATROOM: ")).equals("0")){
+            				chatroom=null;
+            				outputLine="LEFT_CHATROOM: 0\n"+
+            				"JOIN_ID: 0\n";
+            				outStream.write(outputLine.getBytes());
+            				outputLine="CHAT: 0\n"+
+                        	"CLIENT_NAME: "+alias+"\n"+
+                        	"MESSAGE: "+alias+" LEFT\n\n";
+                        	outStream.write(outputLine.getBytes());
+            			//}
+            		}
+            		
+            		else{
             			
             		}
             	}
--- src/main.java
+++ src/main.java
@@ -1,10 +1,4 @@
 
-
-
- 
-
-
- 
 import java.net.*;
 import java.io.*;
  
