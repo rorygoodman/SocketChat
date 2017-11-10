c56c47502c15e09da22b7b9e61927ef446903db4
Rory Goodman <goodmanr@ITSERV.SCSS.TCD.IE>
2017-11-10 17:01:16 +0000

    added to classes

--- src/Chatroom.java
+++ src/Chatroom.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 public class Chatroom {
 	String name;
 	ArrayList<Client> clients;
-	
+	public Chatroom(String name){
+		this.name=name;
+		this.clients = new ArrayList<Client>();
+	}
+	public void addClient(Client newClient){
+		clients.add(newClient);
+	}
 	                                                                  
 }
